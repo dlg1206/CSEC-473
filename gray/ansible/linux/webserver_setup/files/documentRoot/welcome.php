56294c1b43a552b9219deb58cf664d62647c38e4
SIDANSONOFF <89812782+SIDANSONOFF@users.noreply.github.com>
2024-02-15 10:08:40 -0500

    Update welcome.php to pull from correct SQL server

--- gray/ansible/linux/webserver_setup/files/documentRoot/welcome.php
+++ gray/ansible/linux/webserver_setup/files/documentRoot/welcome.php
@@ -12,21 +12,22 @@ if ($_SESSION['loggedin'] !== true) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome Page</title>
+    <title>Authenticated page Page</title>
 </head>
 <body>
     <h1>Welcome</h1>
-    <p>You are logged in! Here are the kareoke songs:</p>
+    <p>You are logged in! Here are the karaoke songs:</p>
 <h1><?php	
-$conn = mysqli_connect("192.168.2.6", "melody", "password", "account");
-$query = "SELECT * FROM User";
+$conn = mysqli_connect("192.168.3.14", "GrayWebserver", "password1", "karaoke");
+$query = "SELECT * FROM Songs";
 if ($result=mysqli_query($conn,$query))
 {
   // Fetch one and one row
   while ($row=mysqli_fetch_row($result))
     {
-        echo " Username :".$row[0]." , ";
-        echo " Password : ".$row[1];
+        echo " Title :".$row[0]." , ";
+        echo " Artist : ".$row[1]." , ";
+        echo " Genre : ".$row[2];
         echo  nl2br (" \n ");
     }//end while
   // Free result set

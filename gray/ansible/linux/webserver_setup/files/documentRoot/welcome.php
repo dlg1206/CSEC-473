2651d2ccc6990bb3050d876a311f699db6a26832
SIDANSONOFF <89812782+SIDANSONOFF@users.noreply.github.com>
2024-02-15 10:21:48 -0500

    Update welcome.php to look like the other pages

--- gray/ansible/linux/webserver_setup/files/documentRoot/welcome.php
+++ gray/ansible/linux/webserver_setup/files/documentRoot/welcome.php
@@ -13,11 +13,30 @@ if ($_SESSION['loggedin'] !== true) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Authenticated page Page</title>
+    <style>
+        /* Add your CSS styles here */
+         body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #1c1425;
+        }
+
+        header {
+            background-color: #1c1425;
+            color: #00000;
+            padding: 1em;
+            text-align: center;
+        }
+    </style>
 </head>
+<header>
+    <h1 style="color:#c28cff;font-family:Verdana;font-size:50px" >Welcome</h1>
+    <p style="color:#c28cff;font-family:Luminari;font-style:italic;font-size:21px" >You are logged in! Here are the karaoke songs:</p>
+</header>
+    
 <body>
-    <h1>Welcome</h1>
-    <p>You are logged in! Here are the karaoke songs:</p>
-<h1><?php	
+<p style="color:#e5c6ef;font-family:Georgia;font-size:19px"><?php	
 $conn = mysqli_connect("192.168.3.14", "GrayWebserver", "password1", "karoake");
 $query = "SELECT * FROM Songs";
 if ($result=mysqli_query($conn,$query))
@@ -25,7 +44,7 @@ if ($result=mysqli_query($conn,$query))
   // Fetch one and one row
   while ($row=mysqli_fetch_row($result))
     {
-        echo " Title :".$row[0]." , ";
+        echo " - Title :".$row[0]." , ";
         echo " Artist : ".$row[1]." , ";
         echo " Genre : ".$row[2];
         echo  nl2br (" \n ");
@@ -34,6 +53,6 @@ if ($result=mysqli_query($conn,$query))
   mysqli_free_result($result);
 }// end if
 mysqli_close($conn);
-?></h1>
+?></p>
 </body>
 </html>
